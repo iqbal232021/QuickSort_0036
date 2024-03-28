e761b2cac26aae4f2b5b6c312a2d5c42821b4dd2
iqbal232021 <furqoniqbal472@gmail.com>
2024-03-28 19:17:14 +0700

    mendapatkan input pengguna

--- onlinepertama/onlinepertama.cpp
+++ onlinepertama/onlinepertama.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 // Array untuk menyimpan elemen
-int data[20];
+int arr[20];
 
 // Menghitung jumlah perbandingan yang dilakukan selama pengurutan
 int comparisonCount = 0;
@@ -12,7 +12,26 @@ int comparisonCount = 0;
 int movementCount = 0;
 int n;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+// Mendapatkan masukan ukuran array yang valid dari pengguna
+void getInput() {
+    while (true) {
+        cout << "Masukkan ukuran array (maksimum 20): ";
+        cin >> n;
+
+        if (n <= 20) {
+            break;
+        }
+        else {
+            cout << "\nUkuran array maksimum adalah 20.\n" << endl;
+        }
+    }
+
+    cout << "\n---\n";
+    cout << "Masukkan Elemen Array\n";
+    cout << "---\n";
+
+    for (int i = 0; i < n; i++) {
+        cout << "Elemen " << i + 1 << ": ";
+        cin >> arr[i];
+    }
 }
