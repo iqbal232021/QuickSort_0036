6537c4c2689811cce53381c94ea8f833948885f0
iqbal232021 <furqoniqbal472@gmail.com>
2024-03-28 19:04:17 +0700

    Add project files.

---
+++ onlinepertama/onlinepertama.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+// Array untuk menyimpan elemen
+int data[20];
+
+// Menghitung jumlah perbandingan yang dilakukan selama pengurutan
+int comparisonCount = 0;
+
+// Menghitung jumlah perpindahan data (swap) yang dilakukan selama pengurutan
+int movementCount = 0;
+int n;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
